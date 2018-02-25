5f11eb85f0e4c3318ea6c980a0183140298a4502
evgenijaZ <evg.zubrich@gmail.com>
2018-02-25 19:34:56 +0200

    added id, name

---
+++ src/main/java/edu/kpi/jee/labs/Category.java
@@ -0,0 +1,14 @@
+package edu.kpi.jee.labs;
+
+/**
+ * @author Yevheniia Zubrych on 25.02.2018.
+ */
+public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
