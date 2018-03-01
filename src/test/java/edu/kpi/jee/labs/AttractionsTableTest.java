e04e95745f8b5665440c604aaf800e7e475ea44b
evgenijaZ <evg.zubrich@gmail.com>
2018-03-01 23:30:08 +0200

    added one more insertion test

--- src/test/java/edu/kpi/jee/labs/AttractionsTableTest.java
+++ src/test/java/edu/kpi/jee/labs/AttractionsTableTest.java
@@ -5,6 +5,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 import java.sql.SQLException;
 
@@ -30,6 +31,15 @@ public class AttractionsTableTest {
         Place place = new Place("Plóshcha Svobodý", "Freedom Square, 8, Kharkiv, Kharkiv region, 61000", -50.004444, -143.766667);
         attractionsTable.insertPlace(place);
     }
+
+    @Test
+    public void insertionTest() throws SQLException{
+        Place expectedPlace = new Place("Potemkin Stairs", "Primorsky Boulevard, 9, Odessa, Ukraine", -46.488122, -149.258856);
+        attractionsTable.insertPlace(expectedPlace);
+        Place receivedPlace = attractionsTable.getPlaceByName(expectedPlace.getName());
+        assertEquals(expectedPlace, receivedPlace);
+    }
+
 
     @After
     public void closeConnection(){
