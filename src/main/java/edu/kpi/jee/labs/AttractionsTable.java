595ee9f5f122066d2123bc893449696f3c1bab7f
evgenijaZ <evg.zubrich@gmail.com>
2018-03-01 22:38:21 +0200

    fixed locale and throwing exceptions

--- src/main/java/edu/kpi/jee/labs/AttractionsTable.java
+++ src/main/java/edu/kpi/jee/labs/AttractionsTable.java
@@ -4,6 +4,7 @@ import edu.kpi.jee.labs.Entities.Category;
 import edu.kpi.jee.labs.Entities.Place;
 
 import java.sql.*;
+import java.util.Locale;
 
 /**
  * @author Yevheniia Zubrych on 25.02.2018.
@@ -31,70 +32,56 @@ public class AttractionsTable {
         }
     }
 
-    public void insertPlace(String name, String address, float latitude, float longitude) {
-        String query = String.format("INSERT INTO `attractions`.`place` (`p_name`, `p_address`, `p_lat`, `p_lng`) VALUES ('%s', '%s', '%-10.6f', '%-10.6f');", name, address, latitude, longitude);
+    public void insertPlace(String name, String address, float latitude, float longitude) throws SQLException {
+        String query = String.format(Locale.US, "INSERT INTO `attractions`.`place` (`p_name`, `p_address`, `p_lat`, `p_lng`, , `p_rating`) VALUES ('%s', '%s', '%-10.6f', '%-10.6f', '0.0');", name, address, latitude, longitude);
         execute(query);
     }
 
-    public void insertPlace(String name, String address, float latitude, float longitude, float rating) {
-        String query = String.format("INSERT INTO `attractions`.`place` (`p_name`, `p_address`, `p_lat`, `p_lng`, `p_rating`) VALUES ('%s', '%s', '%-10.6f', '%-10.6f', '%-3.2f');", name, address, latitude, longitude, rating);
+    public void insertPlace(String name, String address, float latitude, float longitude, float rating) throws SQLException {
+        String query = String.format(Locale.US, "INSERT INTO `attractions`.`place` (`p_name`, `p_address`, `p_lat`, `p_lng`, `p_rating`) VALUES ('%s', '%s', '%-10.6f', '%-10.6f', '%-3.2f');", name, address, latitude, longitude, rating);
         execute(query);
     }
 
-    public void insertPlace(Place place) {
-        String query = String.format("INSERT INTO `attractions`.`place` (`p_name`, `p_address`, `p_lat`, `p_lng`, `p_rating`) VALUES ('%s', '%s', '%-10.6f', '%-10.6f', '%-3.2f');", place.getName(), place.getAddress(), place.getLatitude(), place.getLongitude(), place.getRating());
+    public void insertPlace(Place place) throws SQLException {
+        String query = String.format(Locale.US,"INSERT INTO `attractions`.`place` (`p_name`, `p_address`, `p_lat`, `p_lng`, `p_rating`) VALUES ('%s', '%s', '%10.6f', '%10.6f', '%-3.1f');", place.getName(), place.getAddress(), place.getLatitude(), place.getLongitude(), place.getRating());
         execute(query);
     }
 
-    public void insertCategory(String name) {
-        String query = String.format("INSERT INTO `attractions`.`category` (`c_name`) VALUES ('%s');", name);
+    public void insertCategory(String name) throws SQLException {
+        String query = String.format(Locale.US, "INSERT INTO `attractions`.`category` (`c_name`) VALUES ('%s');", name);
         execute(query);
     }
 
-    public void insertCategory(Category category) {
-        String query = String.format("INSERT INTO `attractions`.`category` (`c_name`) VALUES ('%s');", category.getName());
+    public void insertCategory(Category category) throws SQLException {
+        String query = String.format(Locale.US, "INSERT INTO `attractions`.`category` (`c_name`) VALUES ('%s');", category.getName());
         execute(query);
     }
 
-    public Place getPlaceByName(String name) {
-        String query = String.format("SELECT * FROM `attractions`.`place` WHERE `place`.`p_name` = %s", name);
+    public Place getPlaceByName(String name) throws SQLException {
+        String query = String.format(Locale.US, "SELECT * FROM `attractions`.`place` WHERE `place`.`p_name` = %s", name);
         ResultSet resultSet = executeQuery(query);
-        try {
-            if (resultSet != null) {
-                int id = resultSet.getInt("p_id");
-                String address = resultSet.getString("p_address");
-                float latitude = resultSet.getFloat("p_lat");
-                float longitude = resultSet.getFloat("p_lng");
-                float rating = resultSet.getFloat("p_rating");
-                return new Place(id, name, address, latitude, longitude, rating);
-            } else
-                return null;
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+        if (resultSet != null) {
+            int id = resultSet.getInt("p_id");
+            String address = resultSet.getString("p_address");
+            float latitude = resultSet.getFloat("p_lat");
+            float longitude = resultSet.getFloat("p_lng");
+            float rating = resultSet.getFloat("p_rating");
+            return new Place(id, name, address, latitude, longitude, rating);
+        } else
+            return null;
     }
 
-    private void execute(String query) {
-        try {
-            if (connection != null && !connection.isClosed()) {
-                Statement statement = connection.createStatement();
-                statement.execute(query);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+    private void execute(String query) throws SQLException {
+        if (connection != null && !connection.isClosed()) {
+            Statement statement = connection.createStatement();
+            statement.execute(query);
+        } else throw new IllegalStateException();
     }
 
-    private ResultSet executeQuery(String query) {
-        try {
-            if (connection != null && !connection.isClosed()) {
-                Statement statement = connection.createStatement();
-                return statement.executeQuery(query);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
+    private ResultSet executeQuery(String query) throws SQLException{
+        if (connection != null && !connection.isClosed()) {
+            Statement statement = connection.createStatement();
+            return statement.executeQuery(query);
         }
         return null;
     }
