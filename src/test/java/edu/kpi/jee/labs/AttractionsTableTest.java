c761482f10572f1cd930f36f99db0b74f3a0ced4
evgenijaZ <evg.zubrich@gmail.com>
2018-03-01 22:39:06 +0200

    added insertion test

--- src/test/java/edu/kpi/jee/labs/AttractionsTableTest.java
+++ src/test/java/edu/kpi/jee/labs/AttractionsTableTest.java
@@ -1,13 +1,38 @@
 package edu.kpi.jee.labs;
 
+import edu.kpi.jee.labs.Entities.Place;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.sql.SQLException;
 
 /**
  * @author Yevheniia Zubrych on 01.03.2018.
  */
 
 public class AttractionsTableTest {
-        void rowShouldBeInsertedTest(){
+    private static AttractionsTable attractionsTable;
 
-        }
+    @BeforeClass
+    public static void initTable(){
+        attractionsTable = new AttractionsTable();
+    }
+
+    @Before
+    public void openConnection(){
+        attractionsTable.openConnection();
+    }
+
+    @Test
+    public void placeRowShouldBeInsertedTest() throws SQLException {
+        Place place = new Place("Plóshcha Svobodý", "Freedom Square, 8, Kharkiv, Kharkiv region, 61000", -50.004444, -143.766667);
+        attractionsTable.insertPlace(place);
+    }
+
+    @After
+    public void closeConnection(){
+        attractionsTable.closeConnection();
+    }
 }
