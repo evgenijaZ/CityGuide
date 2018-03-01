1913277aa24fabffc7739045d373769298ad3fa9
evgenijaZ <evg.zubrich@gmail.com>
2018-03-01 15:43:07 +0200

    added insert method

--- src/main/java/edu/kpi/jee/labs/AttractionsTable.java
+++ src/main/java/edu/kpi/jee/labs/AttractionsTable.java
@@ -3,6 +3,7 @@ package edu.kpi.jee.labs;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * @author Yevheniia Zubrych on 25.02.2018.
@@ -25,6 +26,18 @@ public class AttractionsTable {
     public void closeConnection() {
         try {
             connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void insertPlace(String name, String address, float latitude, float longitude) {
+        String query = String.format("INSERT INTO `attractions`.`place` (`p_name`, `p_address`, `p_lat`, `p_lng`) VALUES ('%s', '%s', '%f', '%f');", name, address, latitude, longitude);
+        try {
+            if (connection != null && !connection.isClosed()) {
+                Statement statement = connection.createStatement();
+                statement.execute(query);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
