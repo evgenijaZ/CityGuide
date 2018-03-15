d9ba2bba2469fd204849d296f4bb8056d52bbf9b
evgenijaZ <evg.zubrich@gmail.com>
2018-03-15 15:14:07 +0200

    added CRUD methods

--- src/main/java/edu/kpi/jee/labs/dao/AbstractDAO.java
+++ src/main/java/edu/kpi/jee/labs/dao/AbstractDAO.java
@@ -1,7 +1,22 @@
 package edu.kpi.jee.labs.dao;
 
+import java.util.List;
+
 /**
  * @author Yevheniia Zubrych on 15.03.2018.
  */
-public class AbstractDAO {
+public abstract class AbstractDAO<E, K> {
+
+    public abstract List <E> getAll();
+
+    public abstract boolean update(E entity);
+
+    public abstract E getByKey(K key);
+
+    public abstract boolean delete(E entity);
+
+    public abstract boolean deleteByKey(K key);
+
+    public abstract boolean create(E entity);
+
 }
