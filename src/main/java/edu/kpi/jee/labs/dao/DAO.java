d3125143c10237272a918e95f269ab8677e45daa
evgenijaZ <evg.zubrich@gmail.com>
2018-03-15 22:49:03 +0200

    added delete, create, update methods

--- src/main/java/edu/kpi/jee/labs/dao/DAO.java
+++ src/main/java/edu/kpi/jee/labs/dao/DAO.java
@@ -2,6 +2,7 @@ package edu.kpi.jee.labs.dao;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLDataException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -29,7 +30,7 @@ public class DAO<E, K> implements InterfaceDAO <E, K> {
 
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
-                E entity = (E) Specifics.getEntityFromResultSet(resultSet, tableName);
+                E entity = Specifics.getEntityFromResultSet(resultSet, tableName);
                 if (entity != null)
                     entities.add(entity);
             }
@@ -40,10 +41,9 @@ public class DAO<E, K> implements InterfaceDAO <E, K> {
     }
 
     public boolean update(E entity) {
-        int primaryKey = Specifics.getPrimaryKey(tableName, entity);
         PreparedStatement preparedStatement = handler.getPrepareStatement(Specifics.getUpdateQuery(tableName));
         try {
-            preparedStatement = Specifics.preparedUpdateStatement(dbName, tableName, preparedStatement, entity);
+            preparedStatement = Specifics.prepareStatement(dbName, tableName, preparedStatement, entity);
             return preparedStatement != null && preparedStatement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
@@ -52,20 +52,55 @@ public class DAO<E, K> implements InterfaceDAO <E, K> {
     }
 
     public E getByKey(K key) {
+        List <E> entities = new ArrayList <>();
+        PreparedStatement statement = handler.getPrepareStatement(Specifics.SELECT_BY_ID);
+        try {
+            statement.setString(1, dbName);
+            statement.setString(2, tableName);
+            statement.setObject(3, key);
+
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                E entity = Specifics.getEntityFromResultSet(resultSet, tableName);
+                if (entity != null)
+                    entities.add(entity);
+            }
+            if (entities.size() == 1)
+                return entities.get(0);
+            else throw new SQLDataException("Wrong number of result parameters");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     public boolean delete(E entity) {
-        return false;
-    }
-
-    public boolean deleteByKey(K key) {
+        PreparedStatement statement = handler.getPrepareStatement(Specifics.DELETE_BY_ID);
+        try {
+            statement.setString(1, dbName);
+            statement.setString(2, tableName);
+            statement.setObject(3, Specifics. <E, K>getPrimaryKey(tableName, entity));
+            return statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
     public boolean create(E entity) {
+        K key = Specifics. <E, K>getPrimaryKey(tableName, entity);
+        PreparedStatement statement;
+        if (key != null && key.equals(-1))
+            statement = handler.getPrepareStatement(Specifics.INSERT);
+        else
+            statement = handler.getPrepareStatement(Specifics.INSERT_BY_ID);
+        try {
+            statement = Specifics.prepareStatement(dbName, tableName, statement, entity);
+            return statement != null && statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
-
 
 }
