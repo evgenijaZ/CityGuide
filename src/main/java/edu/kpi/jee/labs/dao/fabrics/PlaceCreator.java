d53de0a0fa691e8f3f3570141b486954ba3b8190
evgenijaZ <evg.zubrich@gmail.com>
2018-03-15 22:47:40 +0200

    added creator implementation

--- src/main/java/edu/kpi/jee/labs/dao/fabrics/PlaceCreator.java
+++ src/main/java/edu/kpi/jee/labs/dao/fabrics/PlaceCreator.java
@@ -1,7 +1,27 @@
 package edu.kpi.jee.labs.dao.fabrics;
 
+import edu.kpi.jee.labs.Entities.Place;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * @author Yevheniia Zubrych on 15.03.2018.
  */
-public class PlaceCreator {
+public class PlaceCreator implements Creator <Place> {
+
+    @Override
+    public Place create(ResultSet resultSet) {
+        Place place = new Place();
+        try {
+            place.setId(resultSet.getInt("id"));
+            place.setName(resultSet.getString("name"));
+            place.setAddress(resultSet.getString("address"));
+            place.setLatitude(resultSet.getFloat("lat"));
+            place.setLongitude(resultSet.getFloat("lng"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return place;
+    }
 }
