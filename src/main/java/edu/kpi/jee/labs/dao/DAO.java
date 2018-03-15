7b7834d8b2ff6491809f0214b5f67685c467c0a3
evgenijaZ <evg.zubrich@gmail.com>
2018-03-16 00:03:04 +0200

    closed prepare statements

--- src/main/java/edu/kpi/jee/labs/dao/DAO.java
+++ src/main/java/edu/kpi/jee/labs/dao/DAO.java
@@ -34,6 +34,7 @@ public class DAO<E, K> implements InterfaceDAO <E, K> {
                 if (entity != null)
                     entities.add(entity);
             }
+            handler.closePrepareStatement(statement);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -41,18 +42,20 @@ public class DAO<E, K> implements InterfaceDAO <E, K> {
     }
 
     public boolean update(E entity) {
-        PreparedStatement preparedStatement = handler.getPrepareStatement(Specifics.getUpdateQuery(tableName));
+        boolean result = false;
+        PreparedStatement statement = handler.getPrepareStatement(Specifics.getUpdateQuery(tableName));
         try {
-            preparedStatement = Specifics.prepareStatement(dbName, tableName, preparedStatement, entity);
-            return preparedStatement != null && preparedStatement.execute();
+            statement = Specifics.prepareStatement(dbName, tableName, statement, entity);
+            result = statement != null && statement.execute();
+            handler.closePrepareStatement(statement);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return result;
     }
 
     public E getByKey(K key) {
-        List <E> entities = new ArrayList <>();
+        E entity = null;
         PreparedStatement statement = handler.getPrepareStatement(Specifics.SELECT_BY_ID);
         try {
             statement.setString(1, dbName);
@@ -60,47 +63,51 @@ public class DAO<E, K> implements InterfaceDAO <E, K> {
             statement.setObject(3, key);
 
             ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                E entity = Specifics.getEntityFromResultSet(resultSet, tableName);
-                if (entity != null)
-                    entities.add(entity);
+            if (resultSet.getMetaData().getColumnCount() != 1)
+                throw new SQLDataException("Wrong number of result parameters");
+            if (resultSet.next()) {
+                entity = Specifics.getEntityFromResultSet(resultSet, tableName);
             }
-            if (entities.size() == 1)
-                return entities.get(0);
-            else throw new SQLDataException("Wrong number of result parameters");
+            handler.closePrepareStatement(statement);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return entity;
     }
 
     public boolean delete(E entity) {
+        boolean result = false;
         PreparedStatement statement = handler.getPrepareStatement(Specifics.DELETE_BY_ID);
         try {
             statement.setString(1, dbName);
             statement.setString(2, tableName);
             statement.setObject(3, Specifics. <E, K>getPrimaryKey(tableName, entity));
-            return statement.execute();
+            result = statement.execute();
+            handler.closePrepareStatement(statement);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return result;
     }
 
     public boolean create(E entity) {
+        boolean result = false;
         K key = Specifics. <E, K>getPrimaryKey(tableName, entity);
         PreparedStatement statement;
-        if (key != null && key.equals(-1))
+        if (key == null)
+            return false;
+        if (key.equals(-1))
             statement = handler.getPrepareStatement(Specifics.INSERT);
         else
             statement = handler.getPrepareStatement(Specifics.INSERT_BY_ID);
         try {
             statement = Specifics.prepareStatement(dbName, tableName, statement, entity);
-            return statement != null && statement.execute();
+            result = statement != null && statement.execute();
+            handler.closePrepareStatement(statement);
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
+        return result;
     }
 
 }
