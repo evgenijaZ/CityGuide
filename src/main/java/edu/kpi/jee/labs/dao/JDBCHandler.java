233ac84730a96f91b90b2cd8fb146da9d40b3f9a
evgenijaZ <evg.zubrich@gmail.com>
2018-03-17 14:53:24 +0200

    reformatted handler class, deleted unused methods

--- src/main/java/edu/kpi/jee/labs/dao/JDBCHandler.java
+++ src/main/java/edu/kpi/jee/labs/dao/JDBCHandler.java
@@ -5,7 +5,7 @@ import java.sql.*;
 /**
  * @author Yevheniia Zubrych on 15.03.2018.
  */
-public class JDBCHandler {
+class JDBCHandler {
 
     private final String URL = "jdbc:mysql://localhost:3306/";
     private final String parameters = "?useUnicode=true&useSSL=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
@@ -14,11 +14,11 @@ public class JDBCHandler {
     private String dbName;
 
 
-    public JDBCHandler(String dbName) {
+    JDBCHandler(String dbName) {
         this.dbName = dbName;
     }
 
-    public Connection openConnection() {
+    private Connection openConnection() {
         try {
             return DriverManager.getConnection(URL + dbName + parameters, USER, PASS);
         } catch (SQLException e) {
@@ -27,45 +27,23 @@ public class JDBCHandler {
         return null;
     }
 
-    public void closeConnection(Connection connection) {
-        if (connection != null)
-            try {
+    private void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed())
                 connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-    }
-
-    public boolean execute(String query) {
-        try (Connection connection = openConnection()) {
-            if (connection != null) {
-                Statement statement = connection.createStatement();
-                return statement.execute(query);
-            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return false;
     }
 
-    public ResultSet executeQuery(String query) {
-        try {
-            Connection connection = openConnection();
-            if (connection != null) {
-                Statement statement = connection.createStatement();
-                return statement.executeQuery(query);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public PreparedStatement getPrepareStatement(String sql) {
+    PreparedStatement getPrepareStatement(String sql) {
         PreparedStatement preparedStatement = null;
+        Connection connection = null;
         try {
-            Connection connection = openConnection();
-            preparedStatement = connection.prepareStatement(sql,Statement.RETURN_GENERATED_KEYS);
+            connection = openConnection();
+            if (connection != null) {
+                preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -73,7 +51,7 @@ public class JDBCHandler {
     }
 
 
-    public void closePrepareStatement(PreparedStatement preparedStatement) {
+    void closePrepareStatement(PreparedStatement preparedStatement) {
         System.out.println(preparedStatement);
         if (preparedStatement != null) {
             try {
